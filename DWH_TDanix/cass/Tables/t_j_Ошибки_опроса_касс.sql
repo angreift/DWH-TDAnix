be64f876b1a34748c49bb5bd2965f744c912631b
angreift <work-targz@ya.ru>
2022-10-03 13:34:53 +0700

    Таблица ошибок обмена с кассами для сохранения случаев, когда касса полностью недоступна

---
+++ DWH_TDanix/cass/Tables/t_j_Ошибки_опроса_касс.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [cass].[t_j_Ошибки_опроса_касс]
+(
+	Код_события int not null identity(1,1),
+	Дата_время_события datetime not null,
+	Код_кассы int not null,
+	Сообщение nvarchar(max) null
+) on [JOURNAL]
