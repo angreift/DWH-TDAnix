68ba198c66c74104b5c9b18faab158c268de67dd
angreift <work-targz@ya.ru>
2022-10-10 10:37:35 +0700

    Блок товародвижения - загрузка и обработка данных

---
+++ DWH_TDanix/td/Tables/t_dim_Причины.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [td].[t_dim_Причины]
+(
+	[Код_причины] smallint not null,
+	[Наименование_причины] nvarchar(50) not null,
+	[Код_категории] smallint not null,
+	[Наименование_категории] nvarchar(50) not null,
+	[Код_в_бухгалтерской_базе] int null,
+	[Заголовок] nvarchar(10) null,
+	[Вид] tinyint null,
+	[Сумма] decimal(10,2) null,
+	[Процент] decimal(10,2) null,
+	[Причина_по_умолчанию_для_отчетов] bit null,
+	[Причина_для_учета_потерь] bit null,
+	[Причина_для_учета_потерь_магазина] bit null,
+	constraint [ix_cl_Код_причины] primary key clustered ([Код_причины] asc) on [DIMENTIONS]
+) on [DIMENTIONS]
--- DWH_TDanix/td/Tables/t_fact_Товародвижение.sql
+++ DWH_TDanix/td/Tables/t_fact_Товародвижение.sql
@@ -2,10 +2,10 @@
 (
 	[Составной_код_документа] NVARCHAR(20) NOT NULL, 
     [Дата] DATE NOT NULL,
-    [Код_магазина] NCHAR(10) NOT NULL, 
+    [Код_магазина] INT NOT NULL, 
     [Склад_в_магазине] INT NULL, 
     [ВидДвижения] TINYINT NULL, 
-    [Код_причины] TINYINT NULL, 
+    [Код_причины] SMALLINT NULL, 
     [Код_товара] BIGINT NOT NULL, 
     [Сумма] DECIMAL(14, 2) NOT NULL, 
     [СуммаЗакупа] DECIMAL(14, 2) NOT NULL,
@@ -13,8 +13,7 @@
     [Цена] DECIMAL(14, 2) NOT NULL, 
     [ЦенаЗакупа] DECIMAL(14, 2) NOT NULL, 
     [ЦенаЗакупаСоСклада] DECIMAL(14, 2) NOT NULL,
-    [Количество] DECIMAL(14, 3) NOT NULL, 
-    CONSTRAINT [PK_t_fact_Товародвижение] PRIMARY KEY CLUSTERED ([Составной_код_документа] ASC) ON [FACTS],
+    [Количество] DECIMAL(14, 3) NOT NULL
 )
 
 GO
@@ -22,5 +21,5 @@ GO
 CREATE NONCLUSTERED INDEX [ix_uncl_Код_товара_Код_магазина] ON [td].[t_fact_Товародвижение] ([Код_товара], [Код_магазина]) ON [FACTS]
 GO
 
-CREATE NONCLUSTERED INDEX [ix_uncl_Дата] ON [td].[t_fact_Товародвижение]  ([Дата]) ON [FACTS]
+CREATE CLUSTERED INDEX [ix_uncl_Дата] ON [td].[t_fact_Товародвижение] ([Дата]) ON [FACTS]
 GO
