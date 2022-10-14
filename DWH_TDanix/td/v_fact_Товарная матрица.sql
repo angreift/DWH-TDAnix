5c39dd110e8af20f3217429ff9983fabfe22149f
inornate74 <inornateman@gmail.com>
2022-10-14 14:30:22 +0700

    Добавлены представления Товарной матрицы с фильтром и без

---
+++ DWH_TDanix/td/v_fact_Товарная матрица.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [td].[v_fact_Товарная матрица]
+	AS
+	select
+	ИД_дата Дата,
+	t_m.Код_магазина,
+	t_t.Код_товара,
+	(
+		select top 1 Признак
+		from
+			td.t_fact_Товарная_матрица ТовМат
+		where
+			ТовМат.Дата <= Дата and
+			ТовМат.Код_магазина = t_m.Код_магазина and
+			ТовМат.Код_товара = t_t.Код_товара 
+			
+			
+	) Признак
+from
+	dbo.t_dim_Календарь 
+cross join
+	(select distinct Код_магазина from td.t_fact_Товарная_матрица) as t_m
+cross join
+	(select distinct Код_товара from td.t_fact_Товарная_матрица) as t_t
+		
---
+++ DWH_TDanix/td/v_fact_Товарная_матрица_фильтр.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [td].[v_fact_Товарная_матрица_фильтр]
+	AS 
+	select
+	ИД_дата Дата,
+	t_m.Код_магазина,
+	t_t.Код_товара,
+	(
+		select top 1 Признак
+		from
+			td.t_fact_Товарная_матрица ТовМат
+		where
+			ТовМат.Дата <= Дата and
+			ТовМат.Код_магазина = t_m.Код_магазина and
+			ТовМат.Код_товара = t_t.Код_товара and
+			ТовМат.Признак in (2,3,4,5,6,9)
+			
+	) Признак
+from
+	dbo.t_dim_Календарь 
+cross join
+	(select distinct Код_магазина from td.t_fact_Товарная_матрица) as t_m
+cross join
+	(select distinct Код_товара from td.t_fact_Товарная_матрица) as t_t
+		
+
