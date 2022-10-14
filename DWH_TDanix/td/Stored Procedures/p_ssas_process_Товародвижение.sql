4ab4e8b5001a38d20d15d78ff4af971234622ee8
angreift <work-targz@ya.ru>
2022-10-14 08:45:35 +0700

    Оптимизация загрузки товародвижения

---
+++ DWH_TDanix/td/Stored Procedures/p_ssas_process_Товародвижение.sql
@@ -0,0 +1,42 @@
+﻿-- =============================================
+-- Author:		kma1860
+-- Create date: 12/10/2022
+-- Description:	Обработка оперативных секций группы мер Детализация чеков
+-- =============================================
+CREATE PROCEDURE [td].[p_ssas_process_Товародвижение]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET DATEFIRST 1;
+
+	declare @command varchar(max), @currDate datetime, @i int;
+
+	set @currDate = getDate();
+	set @currDate = dateAdd(day, (datePart(dw, @currDate) - 1) * -1, @currDate);
+	set @i = 1;
+	while @i >= -7 begin
+		set @command = '
+			<Batch xmlns="http://schemas.microsoft.com/analysisservices/2003/engine">
+			  <Parallel>
+				<Process xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ddl2="http://schemas.microsoft.com/analysisservices/2003/engine/2" xmlns:ddl2_2="http://schemas.microsoft.com/analysisservices/2003/engine/2/2" xmlns:ddl100_100="http://schemas.microsoft.com/analysisservices/2008/engine/100/100" xmlns:ddl200="http://schemas.microsoft.com/analysisservices/2010/engine/200" xmlns:ddl200_200="http://schemas.microsoft.com/analysisservices/2010/engine/200/200" xmlns:ddl300="http://schemas.microsoft.com/analysisservices/2011/engine/300" xmlns:ddl300_300="http://schemas.microsoft.com/analysisservices/2011/engine/300/300" xmlns:ddl400="http://schemas.microsoft.com/analysisservices/2012/engine/400" xmlns:ddl400_400="http://schemas.microsoft.com/analysisservices/2012/engine/400/400" xmlns:ddl500="http://schemas.microsoft.com/analysisservices/2013/engine/500" xmlns:ddl500_500="http://schemas.microsoft.com/analysisservices/2013/engine/500/500">
+				  <Object>
+					<DatabaseID>Холдинг ТД Аникс</DatabaseID>
+					<CubeID>Холдинг ТД Аникс</CubeID>
+					<MeasureGroupID>t Fact Товародвижение</MeasureGroupID>
+					<PartitionID>Товародвижение_' + format(dateadd(day, @i*7, @currDate), 'yyyy') + '_' + format(dateadd(day, @i*7, @currDate), 'MM') + '_' + format(dateadd(day, @i*7, @currDate), 'dd') + '</PartitionID>
+				  </Object>
+				  <Type>ProcessFull</Type>
+				  <WriteBackTableCreation>UseExisting</WriteBackTableCreation>
+				</Process>
+			  </Parallel>
+			</Batch>';
+		print(
+			concat(
+				'Начинается выполнение SSAS команды на сервере SSAS_S19-OLAP. Текст команды: ', char(13), @command
+			)
+		);
+
+		exec(@command) at [SSAS_S19-OLAP];
+		set @i = @i - 1;
+	end;
+END
--- DWH_TDanix/td/Stored Procedures/p_Синхронизация_движений.sql
+++ DWH_TDanix/td/Stored Procedures/p_Синхронизация_движений.sql
@@ -8,15 +8,20 @@ CREATE PROCEDURE [td].[p_Синхронизация_движений]
 AS
 BEGIN
 	SET NOCOUNT ON;
-	Declare @ДатаВыгрузки date, @Выгрузка_при_ЗОД bit, @Код_магазина int
+	if cast(left(@str, 2) as int) < 2 begin
+		print('Too old exchange version. Need at least 02');
+		return 0;
+	end;
+	Declare @ДатаВыгрузки date, @Выгрузка_при_ЗОД bit, @Код_магазина int, @Отпечаток_времени int;
 
 	set @ДатаВыгрузки = '20' + substring(@Str, 3, 6);
 	if substring(@Str, 9, 1) = 'Z' set @Выгрузка_при_ЗОД = 1 else set @Выгрузка_при_ЗОД = 0;
 	set @Код_магазина = cast(substring(@Str, 10, 4) as int);
+	set @Отпечаток_времени = cast(substring(@Str, 14, 6) as int);
 	Insert into [td].[t_raw_Данные_товародвижения_из_магазинов] (
-		[Дата_время], [Дата_выгрузки], [Выгрузка_из_ЗОД], [Data], [Код_магазина]
+		[Дата_время], [Дата_выгрузки], [Выгрузка_из_ЗОД], [Data], [Код_магазина], [Отпечаток_времени]
 	) values (
-		getDate(), @ДатаВыгрузки, @Выгрузка_при_ЗОД, @Str, @Код_магазина
+		getDate(), @ДатаВыгрузки, @Выгрузка_при_ЗОД, @Str, @Код_магазина, @Отпечаток_времени
 	)
 END
 GO
