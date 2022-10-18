31647479c768a8956047bd7b467f06b597cd47c6
inornate74 <inornateman@gmail.com>
2022-10-18 08:48:03 +0700

    Во вьюшках с признаком null=0

--- DWH_TDanix/cass/Views/v_fact_Скидки.sql
+++ DWH_TDanix/cass/Views/v_fact_Скидки.sql
@@ -4,7 +4,7 @@ SELECT        cass.t_fact_Скидки.Код_кассы, cass.t_fact_Скидк
                          cass.t_fact_Скидки.Номер_скидки, cass.t_fact_Скидки.Режим_скидки, cass.t_fact_Скидки.Тип_скидки, cass.t_fact_Скидки.Ставка_скидки, cass.t_fact_Скидки.Сумма_скидки, cass.t_fact_Скидки.Сумма_чека, 
                          cass.t_fact_Скидки.Номер_дисконтной_карты, cass.t_fact_Скидки.Название_дисконтной_карты, cass.t_fact_Скидки.ИД_карты, cass.t_fact_Скидки.Составной_код_кассира, 
                          CAST(CAST(cass.t_fact_Скидки.Дата_время_применения_скидки AS date) AS datetime) AS Дата_применения_скидки, cass.t_dim_Кассы.Код_магазина, cass.t_fact_Детализация_чеков.Составной_код_документа, 
-                         dbo.t_dim_Товары.Код_товара, td.v_fact_Товарная_матрица.Признак, td.v_fact_Товарная_матрица.Код_поставщика
+                         dbo.t_dim_Товары.Код_товара, case when td.v_fact_Товарная_матрица.Признак is null then 0 else td.v_fact_Товарная_матрица.Признак end as Признак, td.v_fact_Товарная_матрица.Код_поставщика
 FROM            cass.t_fact_Скидки INNER JOIN
                          cass.t_dim_Кассы ON cass.t_fact_Скидки.Код_кассы = cass.t_dim_Кассы.Код_кассы INNER JOIN
                          cass.t_fact_Детализация_чеков ON cass.t_fact_Скидки.Составной_код_позиции = cass.t_fact_Детализация_чеков.Составной_код_позиции LEFT OUTER JOIN
