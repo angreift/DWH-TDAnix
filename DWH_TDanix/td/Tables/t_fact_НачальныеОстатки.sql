42ee8b8e85b7d5584b6b6e6bb02fcee114f6eaa5
angreift <work-targz@ya.ru>
2022-10-24 11:08:38 +0700

    Таблица для хранения начальных остатков

---
+++ DWH_TDanix/td/Tables/t_fact_НачальныеОстатки.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [td].[t_fact_НачальныеОстатки]
+(
+	[Дата] DATE NOT NULL, 
+    [Код_товара] BIGINT NOT NULL, 
+    [Код_магазина] INT NOT NULL, 
+    [Код_склада_в_магазине] INT NOT NULL, 
+    [Сумма_закупа] DECIMAL(14, 2) NOT NULL, 
+    [Сумма_закупа_со_склада] DECIMAL(14, 2) NOT NULL, 
+    [Остаток] DECIMAL(14, 3) NOT NULL 
+)
