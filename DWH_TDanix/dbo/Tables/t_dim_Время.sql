94da3af5e3d6f4087a95002bb90e8d9b08e078c0
inornate74 <inornateman@gmail.com>
2022-10-31 16:49:18 +0700

    Создал таблицу t_dim_Время загрузил в нее строки

---
+++ DWH_TDanix/dbo/Tables/t_dim_Время.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[t_dim_Время] (
+    [Время_ИД]                    nvarchar(8)   NOT NULL,
+    [Час]                         nvarchar(2)   NOT NULL,
+    [Минута]                      NVARCHAR (2)  NOT NULL,
+    [Секунда]                     NVARCHAR (2) NOT NULL,
+    
+    CONSTRAINT [PK_t_dim_Время] PRIMARY KEY CLUSTERED ([Время_ИД] ASC) ON [DIMENTIONS]
+) ON [DIMENTIONS];
+
