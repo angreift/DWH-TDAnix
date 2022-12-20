94e773ba830a5a6a214f7483036e2d6562a62af8
angreift <work-targz@ya.ru>
2022-12-20 12:25:26 +0700

    Исправил вьюшку скидок - добавил чувствительность к возвратам

--- DWH_TDanix/cass/Views/v_fact_Скидки.sql
+++ DWH_TDanix/cass/Views/v_fact_Скидки.sql
@@ -1,7 +1,8 @@
 ﻿CREATE VIEW [cass].[v_fact_Скидки]
 AS
 SELECT        cass.t_fact_Скидки.Код_кассы, cass.t_fact_Скидки.ИД_скидки, cass.t_fact_Скидки.Номер_позиции, cass.t_fact_Скидки.Дата_время_применения_скидки, cass.t_fact_Скидки.Объект_скидки, 
-                         cass.t_fact_Скидки.Номер_скидки, cass.t_fact_Скидки.Режим_скидки, cass.t_fact_Скидки.Тип_скидки, cass.t_fact_Скидки.Ставка_скидки, cass.t_fact_Скидки.Сумма_скидки, cass.t_fact_Скидки.Сумма_чека, 
+                         cass.t_fact_Скидки.Номер_скидки, cass.t_fact_Скидки.Режим_скидки, cass.t_fact_Скидки.Тип_скидки, cass.t_fact_Скидки.Ставка_скидки, 
+                         cass.t_fact_Скидки.Сумма_скидки * case when cass.t_fact_Чеки.Возврат = 1 then -1 else 1 end Сумма_скидки, cass.t_fact_Скидки.Сумма_чека, 
                          cass.t_fact_Скидки.Номер_дисконтной_карты, cass.t_fact_Скидки.Название_дисконтной_карты, cass.t_fact_Скидки.ИД_карты, cass.t_fact_Скидки.Составной_код_кассира, 
                          CAST(CAST(cass.t_fact_Скидки.Дата_время_применения_скидки AS date) AS datetime) AS Дата_применения_скидки, cass.t_dim_Кассы.Код_магазина, cass.t_fact_Детализация_чеков.Составной_код_документа, 
                          dbo.t_dim_Товары.Код_товара, 
@@ -19,7 +20,8 @@ SELECT        cass.t_fact_Скидки.Код_кассы, cass.t_fact_Скидк
 FROM            cass.t_fact_Скидки INNER JOIN
                          cass.t_dim_Кассы with (nolock) ON cass.t_fact_Скидки.Код_кассы = cass.t_dim_Кассы.Код_кассы INNER JOIN
                          cass.t_fact_Детализация_чеков ON cass.t_fact_Скидки.Составной_код_позиции = cass.t_fact_Детализация_чеков.Составной_код_позиции LEFT OUTER JOIN
-                         dbo.t_dim_Товары with (nolock) ON cass.t_fact_Скидки.Код_товара = dbo.t_dim_Товары.Код_товара
+                         dbo.t_dim_Товары with (nolock) ON cass.t_fact_Скидки.Код_товара = dbo.t_dim_Товары.Код_товара INNER JOIN
+                         cass.t_fact_Чеки with (nolock) on cass.t_fact_Детализация_чеков.Составной_код_документа = cass.t_fact_Чеки.Составной_код_документа
 GO
 
 
