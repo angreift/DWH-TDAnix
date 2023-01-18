05089abd6d90fa8dc9e83425f1f2938494b69971
angreift <work-targz@ya.ru>
2023-01-18 10:13:19 +0700

    Загрузка Заявка СТ

---
+++ DWH_TDanix/td/Tables/t_fact_Заявка_СТ_шапки.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [td].[t_fact_Заявка_СТ_шапки]
+(	
+	[Составной_код_заявки_СТ] nvarchar(15) not null,
+	[Дата_заявки_СТ] date not null,
+	[Код_магазина] int not null,
+	constraint [PK_Составной_код_Заявка_СТ] primary key clustered ([Составной_код_заявки_СТ] asc) on [FACTS]
+) on [FACTS]
+GO
+
+create nonclustered index [ix_uncl_Дата] on [td].[t_fact_Заявка_СТ_шапки] ([Дата_заявки_СТ]) on [FACTS]
+GO
+create nonclustered index [ix_uncl_Код_магазина] on [td].[t_fact_Заявка_СТ_шапки] ([Код_магазина]) on [FACTS]
+GO
