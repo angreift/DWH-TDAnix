e85b8d082c31c8653a518f4b855b7237af221197
angreift <work-targz@ya.ru>
2023-01-18 10:12:18 +0700

    Загрузка приходных документов

---
+++ DWH_TDanix/td/Tables/t_raw_Данные_ПД_строки.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [td].[t_raw_Данные_ПД_строки]
+(
+	[Дата_время] datetime not null,
+	[Дата_выгрузки] date not null,
+	[Отпечаток_времени] int null,
+	[Выгрузка_из_ЗОД] bit not null,
+	[Код_магазина] int not null,
+	[Data] nvarchar(max) not null
+) on RAW
+GO
+
+create clustered index [ix_cl_Дата_время] on [td].[t_raw_Данные_ПД_строки] (Дата_время ASC) on [RAW]
