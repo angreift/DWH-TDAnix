6da2f8e95679256fea20fa57f76aef0996959b38
angreift <93107031+angreift@users.noreply.github.com>
2023-01-31 11:39:11 +0700

    Версирование таблицы поставщиков и процедуры загрузки

--- DWH_TDanix/dbo/Tables/t_dim_Поставщики.sql
+++ DWH_TDanix/dbo/Tables/t_dim_Поставщики.sql
@@ -1,11 +1,14 @@
-﻿CREATE TABLE [dbo].[t_dim_Поставщики]
+﻿CREATE TABLE [dbo].[t_dim_Поставщики](
+	[Код] [int] NOT NULL,
+	[Наименование] [nvarchar](100) NOT NULL,
+	[ИНН] [bigint] NULL,
+	[КПП] [bigint] NULL,
+	[Адрес] [nvarchar](200) NULL,
+	[Ответственное_лицо] [nvarchar](100) NULL,
+	[Автозаявка] [bit] NULL,
+PRIMARY KEY CLUSTERED 
 (
-	[Код] INT NOT NULL PRIMARY KEY, 
-    [Наименование] nvarchar(100) NOT NULL,
-    [ИНН] BIGINT NULL, 
-    [КПП] BIGINT NULL, 
-    [Адрес] nvarchar(200) NULL, 
-    [Ответственное_лицо] nvarchar(100) NULL, 
-    [Автозаявка] BIT NULL
-
-) on [DIMENTIONS];
+	[Код] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [DIMENTIONS]
+) ON [DIMENTIONS]
+GO
