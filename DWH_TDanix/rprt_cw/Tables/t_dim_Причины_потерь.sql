4c2996213f483ffebcb10ba5d26447130dcdb0cc
inornate74 <inornateman@gmail.com>
2023-02-01 14:09:23 +0700

    Создание/изменение таблиц и индексов для Распичинивания потерь

--- DWH_TDanix/rprt_cw/Tables/t_dim_Причины_потерь.sql
+++ DWH_TDanix/rprt_cw/Tables/t_dim_Причины_потерь.sql
@@ -1,7 +1,9 @@
-﻿/*CREATE TABLE [rprt_cw].[t_dim_Причины_потерь]
-(
-	[Подпричина] varchar(30),
-	[Причина] varchar(30), 
-    [Приоритет] VARCHAR(30) NULL
-)
-ON DIMENTIONS;*/
+﻿
+CREATE TABLE [rprt_cw].[t_dim_Причины_потерь](
+	[Подпричина] [varchar](30) NULL,
+	[Причина] [varchar](30) NULL,
+	[Приоритет] [varchar](30) NULL,
+	[Код_причины] [tinyint] NULL
+) ON [DIMENTIONS]
+GO
+
---
+++ DWH_TDanix/rprt_cw/Tables/t_fact_Распричинивание_потерь.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [rprt_cw].[t_fact_Распричинивание_потерь](
+	[Дата] [date] NOT NULL,
+	[Код_магазина] [int] NOT NULL,
+	[Код_товара] [bigint] NOT NULL,
+	[Код_причины] [tinyint] NOT NULL,
+	[Влияние] [decimal](14, 2) NULL,
+	[Флаг] [bit] NOT NULL
+) ON [REPORTS]
+GO
+CREATE NONCLUSTERED INDEX [ix_uncl_КодТовара_КодМагазина] ON [rprt_cw].[t_fact_Распричинивание_потерь]
+(
+	[Код_товара] ASC,
+	[Код_магазина] ASC
+)
+go
+
+CREATE CLUSTERED INDEX [ix_cl_Дата] ON [rprt_cw].[t_fact_Распричинивание_потерь]
+(
+	[Дата] desc
+) on [REPORTS]
+GO
