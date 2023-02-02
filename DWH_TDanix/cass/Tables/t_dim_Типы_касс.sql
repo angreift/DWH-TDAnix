1247ba9bd6debee3f68de4ecfa308e1aa7a4ac34
BitLittel <opera.operaciy@yandex.ru>
2023-02-02 12:47:59 +0700

    Исправления размера типа данных в кассах. Добавлена таблица с типами касс. Удалён уникальный не класстерезованный индекс с касс по IP.

---
+++ DWH_TDanix/cass/Tables/t_dim_Типы_касс.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [cass].[t_dim_Типы_касс]
+(
+	[Код_типа_кассы] NVARCHAR(7) NOT NULL,
+	[Наименование] NVARCHAR(30) NULL,
+	CONSTRAINT [PK_t_dim_Типы_касс] PRIMARY KEY CLUSTERED ([Код_типа_кассы] ASC) ON [DIMENTIONS]
+) ON [DIMENTIONS];
