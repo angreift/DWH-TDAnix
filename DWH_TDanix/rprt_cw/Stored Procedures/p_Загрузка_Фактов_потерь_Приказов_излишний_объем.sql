6900056d3433a27bab5f9fdb741e62496fc6a13b
inornate74 <inornateman@gmail.com>
2023-02-16 10:35:46 +0700

    Добавил процедуру Загрузка Фактов потерь и приказов излишний объем и вьюшку Распричинивание потерь

---
+++ DWH_TDanix/rprt_cw/Stored Procedures/p_Загрузка_Фактов_потерь_Приказов_излишний_объем.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [rprt_cw].[p_Загрузка_Фактов_потерь_Приказов_излишний_объем]
+
+AS BEGIN
+
+declare @dateStart date, @dateEnd date
+set @dateStart=dateadd(d, -8, getdate())
+set @dateEnd=dateadd(d,-1,getdate())
+
+delete from rprt_cw.t_fact_Факты_потерь where Дата between @dateStart and @dateEnd --очистка Фактов потерь за неделю
+
+insert into rprt_cw.t_fact_Факты_потерь(Дата, Код_магазина, Код_товара, Сумма, Количество) --загрузка Фактов потерь за неделю
+select тд.Дата, тд.Код_магазина, тд.Код_товара, sum(тд.СуммаЗакупаСоСклада), sum(Количество)
+FROM [DWH].[dbo].[v_fact_Товародвижение] as ТД
+	left join dbo.t_dim_Товары Т on т.Код_товара=тд.Код_товара
+where (тд.Дата between @dateStart and @dateEnd) and Количество!=0 and  Количество is not null and Код_причины in(12,42,175,18,17) and т.Код_группы not in(5815,24466,17993,37,39132)
+group by тд.Дата, тд.Код_магазина, тд.Код_товара
+having sum(тд.СуммаЗакупаСоСклада)!=0 and sum(тд.Количество)!=0
+
+truncate table rprt_cw.t_fact_Приказы_Излишний_объем --удаление Приказов излишний объем
+
+insert into rprt_cw.t_fact_Приказы_Излишний_объем --загрука Приказов излишний объем из ОТБРС
+select cast(sp1760 as date), sp1761, sp65 from Rozn.rozn.dbo.DH1769 where SP1764='     P   '
+
+END
---
+++ DWH_TDanix/rprt_cw/Views/v_fact_Распричинивание_Потерь.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [rprt_cw].[v_fact_Распричинивание_потерь]
+AS
+SELECT        rprt_cw.t_fact_Распричинивание_потерь.Дата, dbo.t_dim_Магазины.Код AS Код_магазина, dbo.t_dim_Магазины.Наименование AS Магазин,  dbo.t_dim_Товары.Код_товара,  dbo.t_dim_Товары.Наименование AS Товар, 
+						 rprt_cw.t_fact_Факты_потерь.Сумма, rprt_cw.t_fact_Факты_потерь.Количество, 
+                         rprt_cw.t_dim_Причины_потерь.Причина, rprt_cw.t_dim_Причины_потерь.Подпричина,
+                         rprt_cw.t_fact_Распричинивание_потерь.Код_причины
+FROM            rprt_cw.t_fact_Распричинивание_потерь INNER JOIN
+                         rprt_cw.t_dim_Причины_потерь ON rprt_cw.t_fact_Распричинивание_потерь.Код_причины = rprt_cw.t_dim_Причины_потерь.Код_причины INNER JOIN
+                         dbo.t_dim_Товары ON rprt_cw.t_fact_Распричинивание_потерь.Код_товара = dbo.t_dim_Товары.Код_товара INNER JOIN
+                         dbo.t_dim_Магазины ON rprt_cw.t_fact_Распричинивание_потерь.Код_магазина = dbo.t_dim_Магазины.Код INNER JOIN
+                         rprt_cw.t_fact_Факты_потерь ON rprt_cw.t_fact_Распричинивание_потерь.Дата = rprt_cw.t_fact_Факты_потерь.Дата AND 
+                         rprt_cw.t_fact_Распричинивание_потерь.Код_магазина = rprt_cw.t_fact_Факты_потерь.Код_магазина AND rprt_cw.t_fact_Распричинивание_потерь.Код_товара = rprt_cw.t_fact_Факты_потерь.Код_товара
+GO
