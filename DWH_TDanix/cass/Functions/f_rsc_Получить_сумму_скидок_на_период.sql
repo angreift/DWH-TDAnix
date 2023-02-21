12d1d443db60691361f9c98d90758868a0f59eb0
angreift <93107031+angreift@users.noreply.github.com>
2023-02-21 13:59:47 +0700

    Добавил функцию для РСЦ, возращающую сумму скидок за период в разрезе магазинов и групп

---
+++ DWH_TDanix/cass/Functions/f_rsc_Получить_сумму_скидок_на_период.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		kma1860
+-- Create date: 21/02/2023
+-- Description:	Возвращает сумму скидки на товары (Запрос от Лизы Чернцовой)
+-- =============================================
+CREATE FUNCTION cass.f_rsc_Получить_сумму_скидок_на_период 
+(	
+	@DateStart date, @DateEnd date
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	select
+		sum(Сумма_скидки) SummDiscount,
+		м.Код CodeMag,
+		т.Код_группы CodeGroup
+	from cass.t_fact_Детализация_чеков д
+	join cass.t_dim_Кассы к on д.Код_кассы = к.Код_кассы
+	join dbo.t_dim_Магазины м on к.Код_магазина = м.Код
+	join dbo.t_dim_Товары т on д.Код_товара = т.Код_товара
+	where д.Дата_добавления_позиции >= @DateStart and д.Дата_добавления_позиции <= @DateEnd and м.Группа in ('Розница', 'РС Закрытые')
+	group by м.Код, т.Код_группы
+)
+GO
