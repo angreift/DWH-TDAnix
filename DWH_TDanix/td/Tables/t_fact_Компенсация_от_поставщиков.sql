6f81c45298d4dfe51c8788b522d2ff256442d232
inornate74 <84286381+inornate74@users.noreply.github.com>
2023-03-10 12:47:29 +0700

    Изменил типы данных в таблице Компенсация, добавил ГМ в куб; Изменил Пенетрация ПРОЦ

--- DWH_TDanix/td/Tables/t_fact_Компенсация_от_поставщиков.sql
+++ DWH_TDanix/td/Tables/t_fact_Компенсация_от_поставщиков.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [td].[t_fact_Компенсация_от_поставщиков](
 	[Код_магазина] [int] NOT NULL,
-	[Код_товара] [int] NOT NULL,
+	[Код_товара] [bigint] NOT NULL,
 	[Код_поставщика] [int] NOT NULL,
 	[Дата] [date] NOT NULL,
-	[Сумма] [money] NOT NULL
+	[Сумма] DECIMAL(14, 2) NOT NULL
 ) ON [FACTS]
 GO
 
