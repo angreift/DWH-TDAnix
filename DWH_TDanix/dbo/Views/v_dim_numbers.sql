133eef94ce0be705703c3f1831136538ae6e2809
angreift <93107031+angreift@users.noreply.github.com>
2023-03-17 10:21:06 +0700

    Связал детализацию чеков и чеки со скидочные карты через многие ко многим

---
+++ DWH_TDanix/dbo/Views/v_dim_numbers.sql
@@ -0,0 +1,13 @@
+﻿declare @q nvarchar(max)
+
+if (SELECT count(*) FROM sys.sql_modules WHERE object_id = OBJECT_ID('dbo.v_dim_numbers')) = 0 begin
+	set @q = 'drop view if exists [dbo].[v_dim_numbers]'
+	exec(@q)
+	
+	set @q = '
+		CREATE VIEW [dbo].[v_dim_numbers]
+		AS select cast([number] as smallint) number from [master]..[spt_values] where [type] = ''P''
+	'
+	exec(@q)
+end 
+go
