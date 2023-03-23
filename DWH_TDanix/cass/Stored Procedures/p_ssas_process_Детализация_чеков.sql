435099925fb212e74821a2023831e4493c501244
angreift <93107031+angreift@users.noreply.github.com>
2023-03-23 14:33:42 +0700

    В процедуру процессингда детазизации чеков добавил процессинг детализации SKU

--- DWH_TDanix/cass/Stored Procedures/p_ssas_process_Детализация_чеков.sql
+++ DWH_TDanix/cass/Stored Procedures/p_ssas_process_Детализация_чеков.sql
@@ -71,9 +71,60 @@ BEGIN
 
 	print(
 		concat(
-			'Начинается выполнениу SSAS команды на сервере SSAS_S19-OLAP. Текст команды: ', char(13), @command
+			'Начинается выполнение SSAS команды на сервере SSAS_S19-OLAP. Текст команды: ', char(13), @command
 		)
 	);
 
 	exec(@command) at [SSAS_S19-OLAP];
+
+	-- Детализация чеков SKU
+
+	set @command = '
+		<Batch xmlns="http://schemas.microsoft.com/analysisservices/2003/engine">
+		  <Parallel>
+			<Process xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ddl2="http://schemas.microsoft.com/analysisservices/2003/engine/2" xmlns:ddl2_2="http://schemas.microsoft.com/analysisservices/2003/engine/2/2" xmlns:ddl100_100="http://schemas.microsoft.com/analysisservices/2008/engine/100/100" xmlns:ddl200="http://schemas.microsoft.com/analysisservices/2010/engine/200" xmlns:ddl200_200="http://schemas.microsoft.com/analysisservices/2010/engine/200/200" xmlns:ddl300="http://schemas.microsoft.com/analysisservices/2011/engine/300" xmlns:ddl300_300="http://schemas.microsoft.com/analysisservices/2011/engine/300/300" xmlns:ddl400="http://schemas.microsoft.com/analysisservices/2012/engine/400" xmlns:ddl400_400="http://schemas.microsoft.com/analysisservices/2012/engine/400/400" xmlns:ddl500="http://schemas.microsoft.com/analysisservices/2013/engine/500" xmlns:ddl500_500="http://schemas.microsoft.com/analysisservices/2013/engine/500/500">
+			  <Object>
+				<DatabaseID>Холдинг ТД Аникс</DatabaseID>
+				<CubeID>Холдинг ТД Аникс</CubeID>
+				<MeasureGroupID>Детализация Чеков</MeasureGroupID>
+				<PartitionID>Детализация_Чеков_%prevPartitionName%</PartitionID>
+			  </Object>
+			  <Type>ProcessFull</Type>
+			  <WriteBackTableCreation>UseExisting</WriteBackTableCreation>
+			</Process>
+			<Process xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ddl2="http://schemas.microsoft.com/analysisservices/2003/engine/2" xmlns:ddl2_2="http://schemas.microsoft.com/analysisservices/2003/engine/2/2" xmlns:ddl100_100="http://schemas.microsoft.com/analysisservices/2008/engine/100/100" xmlns:ddl200="http://schemas.microsoft.com/analysisservices/2010/engine/200" xmlns:ddl200_200="http://schemas.microsoft.com/analysisservices/2010/engine/200/200" xmlns:ddl300="http://schemas.microsoft.com/analysisservices/2011/engine/300" xmlns:ddl300_300="http://schemas.microsoft.com/analysisservices/2011/engine/300/300" xmlns:ddl400="http://schemas.microsoft.com/analysisservices/2012/engine/400" xmlns:ddl400_400="http://schemas.microsoft.com/analysisservices/2012/engine/400/400" xmlns:ddl500="http://schemas.microsoft.com/analysisservices/2013/engine/500" xmlns:ddl500_500="http://schemas.microsoft.com/analysisservices/2013/engine/500/500">
+			  <Object>
+				<DatabaseID>Холдинг ТД Аникс</DatabaseID>
+				<CubeID>Холдинг ТД Аникс</CubeID>
+				<MeasureGroupID>Детализация Чеков</MeasureGroupID>
+				<PartitionID>Детализация_Чеков_%currPartitionName%</PartitionID>
+			  </Object>
+			  <Type>ProcessFull</Type>
+			  <WriteBackTableCreation>UseExisting</WriteBackTableCreation>
+			</Process>
+			<Process xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ddl2="http://schemas.microsoft.com/analysisservices/2003/engine/2" xmlns:ddl2_2="http://schemas.microsoft.com/analysisservices/2003/engine/2/2" xmlns:ddl100_100="http://schemas.microsoft.com/analysisservices/2008/engine/100/100" xmlns:ddl200="http://schemas.microsoft.com/analysisservices/2010/engine/200" xmlns:ddl200_200="http://schemas.microsoft.com/analysisservices/2010/engine/200/200" xmlns:ddl300="http://schemas.microsoft.com/analysisservices/2011/engine/300" xmlns:ddl300_300="http://schemas.microsoft.com/analysisservices/2011/engine/300/300" xmlns:ddl400="http://schemas.microsoft.com/analysisservices/2012/engine/400" xmlns:ddl400_400="http://schemas.microsoft.com/analysisservices/2012/engine/400/400" xmlns:ddl500="http://schemas.microsoft.com/analysisservices/2013/engine/500" xmlns:ddl500_500="http://schemas.microsoft.com/analysisservices/2013/engine/500/500">
+			  <Object>
+				<DatabaseID>Холдинг ТД Аникс</DatabaseID>
+				<CubeID>Холдинг ТД Аникс</CubeID>
+				<MeasureGroupID>Детализация Чеков</MeasureGroupID>
+				<PartitionID>Детализация_Чеков_%nextPartitionName%</PartitionID>
+			  </Object>
+			  <Type>ProcessFull</Type>
+			  <WriteBackTableCreation>UseExisting</WriteBackTableCreation>
+			</Process>
+		  </Parallel>
+		</Batch>
+	';
+	set @command = replace(@command, '%prevPartitionName%', @prevPartitionName);
+	set @command = replace(@command, '%currPartitionName%', @currPartitionName);
+	set @command = replace(@command, '%nextPartitionName%', @nextPartitionName);
+
+	print(
+		concat(
+			'Начинается выполнение SSAS команды на сервере SSAS_S19-OLAP. Текст команды: ', char(13), @command
+		)
+	);
+
+	exec(@command) at [SSAS_S19-OLAP];
+
 END
